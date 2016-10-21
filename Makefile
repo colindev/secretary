aca514694dde79e080311905d09825866597f720
colin <colin1124x@gmail.com>
2016-10-21 15:39:05 +0800

    調整檔案配置

--- Makefile
+++ Makefile
@@ -4,5 +4,34 @@ PWD := $(PWD)
 TAG := `git describe --tags | cut -d '-' -f 1 `.`git rev-parse --short HEAD`
 DATETIME := `TZ=$(TZ) date +%Y%m%d.%H%M%S`
 
-build:
-	$(GO) build -ldflags "-X main.Version=$(TAG) -X main.CompileDate=$(DATETIME)" -a -o ./secretary
+APP := secretary
+DESCRIPTION := 'secretary - 仿 cron 排程工具'
+SERVICE_FILE := /lib/systemd/system/$(APP).service
+
+test:
+	$(GO) test -v ./process
+
+build: test
+	$(GO) build -ldflags "-X main.Version=$(TAG) -X main.CompileDate=$(DATETIME)" -a -o ./$(APP)
+
+deploy:
+	@read -r -p "deoloy dist: ex. user@127.0.0.1:/dir: " DIST; if [ -n "$$DIST" ] ; then scp -r ./$(APP) ./Makefile ./custom/{.env.sample,schedule.sample} ./util $$DIST; fi
+
+service:
+	mkdir -p ./custom/util/systemd
+	cat util/systemd/service.temp |\
+		sed 's@{CMD}@'"$(APP)"'@g' |\
+		sed 's/{DESCRIPTION}/'"$(DESCRIPTION)"'/' |\
+		sed "s/{USER}/$(USER)/g" |\
+		sed 's@{PWD}@'"$(PWD)"'@g' > custom/util/systemd/$(APP).service
+
+install-service:
+	cp custom/util/systemd/$(APP).service $(SERVICE_FILE)
+	systemctl daemon-reload
+	systemctl enable $(APP)
+
+uninstall-service:
+	systemctl stop $(APP)
+	systemctl disable $(APP)
+	rm -i $(SERVICE_FILE)
+	systemctl daemon-reload
--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/joho/godotenv"
+
 	"rde-tech.vir888.com/dev/secretary/secretary.git/parser"
 	"rde-tech.vir888.com/dev/secretary/secretary.git/process"
 	"rde-tech.vir888.com/dev/secretary/secretary.git/worker"
@@ -26,6 +28,7 @@ var (
 	CompileDate string
 	// Env collect flag args
 	Env = struct {
+		path           string
 		AdminAPIAddr   string
 		AdminAPIPrefix string
 		Interval       int64
@@ -33,17 +36,19 @@ var (
 		Backup         string
 		BackupInterval int
 	}{}
+
 	prc *process.Process
 )
 
-func main() {
+func init() {
 
+	flag.StringVar(&Env.path, "env", "", "env filename")
 	flag.StringVar(&Env.AdminAPIAddr, "api.addr", "", "開發測試用(預計廢除)")
 	flag.StringVar(&Env.AdminAPIPrefix, "api.prefix", "", "API 路徑前綴")
-	flag.Int64Var(&Env.Interval, "interval", 10, "排程掃描間隔")
+	flag.Int64Var(&Env.Interval, "interval", 0, "排程掃描間隔")
 	flag.StringVar(&Env.Schedule, "schedule", "", "排程設定檔")
-	flag.StringVar(&Env.Backup, "backup", "/tmp/schedule.backup", "排程備份位置(僅在啟動API時才有用)")
-	flag.IntVar(&Env.BackupInterval, "backup.interval", 300, "備份間隔秒數(僅在啟動API時才有用)")
+	flag.StringVar(&Env.Backup, "backup", "", "排程備份位置(僅在啟動API時才有用)")
+	flag.IntVar(&Env.BackupInterval, "backup.interval", 0, "備份間隔秒數(僅在啟動API時才有用)")
 
 	var showVersion = flag.Bool("v", false, "display current version")
 	flag.Parse()
@@ -53,9 +58,37 @@ func main() {
 		os.Exit(0)
 	}
 
+	if err := godotenv.Load(Env.path); err == nil {
+		if Env.AdminAPIAddr == "" {
+			Env.AdminAPIAddr = os.Getenv("ADMIN_API_ADDR")
+		}
+		if Env.AdminAPIPrefix == "" {
+			Env.AdminAPIPrefix = os.Getenv("ADMIN_API_PREFIX")
+		}
+		if Env.Interval == 0 {
+			Env.Interval, _ = strconv.ParseInt(os.Getenv("INTERVAL"), 10, 64)
+		}
+		if Env.Schedule == "" {
+			Env.Schedule = os.Getenv("SCHEDULE")
+		}
+		if Env.Backup == "" {
+			Env.Backup = os.Getenv("BACKUP")
+		}
+		if Env.BackupInterval == 0 {
+			Env.BackupInterval, _ = strconv.Atoi(os.Getenv("BACKUP_INTERVAL"))
+		}
+	}
+
+	log.Println("env:", Env.path)
 	log.Println("version:", Version, CompileDate)
 	log.Println("interval:", Env.Interval)
 	log.Println("schedule:", Env.Schedule)
+	if Env.Interval < 1 {
+		panic("interval 不可小於 1")
+	}
+}
+
+func main() {
 
 	// 建構並初始化排程
 	prc = process.New(log.New(os.Stdout, "[process]", log.Lshortfile|log.LstdFlags))
@@ -79,12 +112,19 @@ func main() {
 	// TODO: 評估要不要拿掉
 	if Env.AdminAPIAddr != "" {
 		http.Handle(Env.AdminAPIPrefix+"/", http.StripPrefix(Env.AdminAPIPrefix, CreateRESTHandler()))
-		go http.ListenAndServe(Env.AdminAPIAddr, nil)
+		go func() {
+			err := http.ListenAndServe(Env.AdminAPIAddr, nil)
+			if err != nil {
+				log.Println("[http] ", err)
+			}
+		}()
 		log.Println("admin api:", Env.AdminAPIAddr, Env.AdminAPIPrefix)
 
-		// 背景備份, 僅在啟用遠端 API 時才需要啟動
-		log.Println("backup:", Env.Backup, time.Duration(Env.BackupInterval)*time.Second)
-		prc.Backup(Env.Backup, time.Duration(Env.BackupInterval)*time.Second)
+		// 背景備份, 僅在啟用遠端 API, 且備份間隔大於0 時才需要啟動
+		if Env.BackupInterval > 0 {
+			log.Println("backup:", Env.Backup, time.Duration(Env.BackupInterval)*time.Second)
+			prc.Backup(Env.Backup, time.Duration(Env.BackupInterval)*time.Second)
+		}
 	}
 
 	// listen os signal
